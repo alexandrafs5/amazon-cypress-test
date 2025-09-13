d16027d2c01eb6ab653ddbe3d1e82927b7cbe9d6
alexandrafs5 <alexandra051009@gmail.com>
2025-09-13 16:57:04 -0700

    separate page object from spec, improve selectors and timeouts

--- cypress/support/pageObjects/AmazonPage.js
+++ cypress/support/pageObjects/AmazonPage.js
@@ -4,33 +4,30 @@ class AmazonPage {
     }
 
     searchProduct(productName) {
-        cy.get('#twotabsearchtextbox', { timeout: 10000 })
+        cy.get('#twotabsearchtextbox', { timeout: 20000 })
             .should('be.visible')
             .type(`${productName}{enter}`);
     }
 
     selectFirstProduct() {
         cy.get('.s-main-slot .s-result-item', { timeout: 20000 })
-            .should('be.visible')
-            .then(($items) => {
-                const validItems = $items.filter((index, el) => el.querySelector('h2 a'));
-                if (validItems.length > 0) {
-                    cy.wrap(validItems[0]).find('h2 a').first().click();
-                } else {
-                    throw new Error('No product found with h2 a');
-                }
-            });
+            .filter(':has(h2 a)')
+            .first()
+            .find('h2 a')
+            .first()
+            .click();
     }
 
     addToCart() {
-        cy.get('#add-to-cart-button', { timeout: 10000 })
+        cy.contains('Add to Cart', { timeout: 15000 })
             .should('be.visible')
             .click();
     }
 
     verifyAdded() {
-        cy.get('#huc-v2-order-row-confirm-text', { timeout: 10000 })
-            .should('contain.text', 'added to Cart');
+        cy.get('h1, span, div', { timeout: 15000 })
+            .contains(/added to Cart|Added to Cart/i)
+            .should('exist');
     }
 }
 
