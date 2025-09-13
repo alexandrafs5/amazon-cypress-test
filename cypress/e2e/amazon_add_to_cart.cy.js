f8ffd2eab8bd5962768e256f53605ff46537ee91
alexandrafs5 <alexandra051009@gmail.com>
2025-09-13 16:12:35 -0700

    Add Amazon Add-to-Cart Cypress test

--- cypress/e2e/amazon_add_to_cart.cy.js
+++ cypress/e2e/amazon_add_to_cart.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import AmazonPage from '../support/pageObjects/AmazonPage';
+
+describe('Amazon Add to Cart Test', () => {
+  const amazon = new AmazonPage();
+
+  it('adds a product to cart successfully', () => {
+    amazon.visit();
+    amazon.searchProduct('laptop');  // o cualquier producto de prueba
+    amazon.selectFirstProduct();
+    amazon.addToCart();
+    amazon.verifyAdded();
+  });
+});
